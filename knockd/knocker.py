60f32f22a9965a1587b5a88520fc8253317bd3c4
Swapnil Kumbhar <swapsk05@gmail.com>
2019-04-07 12:35:42 +0530

    SK: Added arg, config parse, sanity check

--- knockd/knocker.py
+++ knockd/knocker.py
@@ -1,23 +1,12 @@
 #!/usr/bin/env python3
+from argparse import ArgumentParser
+from configparser import ConfigParser
 import pyotp
-import sys
 import time
 import subprocess
-
-
-conf_iptables = """
-[options]
-    logfile = /var/log/knockd.log
-    Interface = ens160
-
-[TOTP]
-    sequence = {0},{1}
-    tcpflags = syn
-    seq_timeout = 5
-    start_command = /sbin/iptables -F;/sbin/iptables -A INPUT -p tcp --dport {2} -j ACCEPT
-    cmd_timeout = 22
-    stop_command = /sbin/iptables -D INPUT -p tcp --dport {2} -j ACCEPT; /sbin/iptables -A INPUT -p tcp --dport {2} -j REJECT
-"""
+import platform
+import os
+import shutil
 
 
 def get_four_digit(knock):
@@ -34,17 +23,94 @@ def get_knocks(code):
     return first_knock, second_knock
 
 
-def write_to_knockd_conf(first_knock, second_knock, port, mode):
-    if mode == "iptables":
-        global conf_iptables
-        conf_iptables_formatted = conf_iptables.format(
-            str(first_knock),
-            str(second_knock),
-            str(port))
-        with open("/etc/knockd.conf", "w+") as cnf:
-            cnf.write(conf_iptables_formatted)
+def check_sanity(config_path):
+    """
+    Function to check if the program won't crash when it is run.
+    Following are the parameters that are checked for:
+        - Checks if NOT Windows
+        - Checks if root
+        - Checks if knockd service exists
+        - Checks if iptables exists
+        - Checks if specified config file exists and is accessible
+        - Checks if config file is valid (Syntax)
+        - Checks if the "totp_secret" option of the config file is filled
+    """
+    os_name = platform.system()
+    print("[+] Detecting operating system...")
+    if os_name == "Windows":
+        win_32 = platform.win32_ver()
+        print("----> [!] Detected Windows: {} {} {}".format(win_32[0],
+                                                            win_32[1],
+                                                            win_32[2]))
+        print("----> [!] We do not support Windows (yet).")
+        print("----> [!] Exiting...")
+        exit()
+    else:
+        print("----> [+] Detected OS: {}".format(os_name))
+        print("[+] Checking for root privileges...")
+        if os.geteuid() != 0:
+            print("----> [!] Please run this file as root")
+            print("----> [!] Exiting...")
+            exit()
+        else:
+            print("----> [+] Looks good!")
+        print("[+] Checking if 'knockd' exists...")
+        if shutil.which("knockd") is None:
+            print("----> [!] Knockd is not installed or not found")
+            print("----> [!] Please install and then run")
+            print("----> [!] Exiting...")
+            exit()
+        else:
+            print("----> [+] Looks good!")
+        print("[+] Checking if iptables exists...")
+        if shutil.which("iptables") is None:
+            print("----> [!] iptables was not detected")
+            print("----> [!] Exiting...")
+            exit()
+        else:
+            print("----> [!] Looks good!")
+        print("[+] Checking config file")
+        try:
+            config_file = open(config_path, "r")
+            print("----> [+] Found file: {}".format(config_path))
+            config_file.close()
+        except Exception as e:
+            print("----> [!] Somethign unforseen happened.")
+            print("----> [!] Exception: {}".format(e))
+            print("----> [!] Exiting...")
+            exit()
+        print("[+] Checking config file")
+        try:
+            config_args = ConfigParser()
+            config_args.read(config_path)
+            print("----> [+] Successfully parsed config file")
+            print("[+] Checking parsed values for empty entries")
+            for section in config_args.sections():
+                print("[+] Checking section: {}".format(section))
+                sec = config_args[section]
+                for key in list(config_args[section]):
+                    print("--------> [+] Checking {}".format(key))
+                    if sec[key] == "":
+                        print("------------> [!] Empty!")
+                        print("------------> [!] Please fill: {}".format(key))
+                        exit()
+                    else:
+                        print("------------> [+] Looks good!")
+            print("[+] Checking secret...")
+            if len(config_args['KNOCKER']['totp_secret']) != 16:
+                print("----> [!] Secret does not seem right! Please check it.")
+                print("----> [!] Run generate_auth.py to create a new secret")
+                print("----> [!] Exiting...")
+                exit()
+            else:
+                print("----> [+] Looks good!")
+        except Exception as e:
+            print("----> [!] Something unforseen happened.")
+            print("----> [!] Exception: {}".format(e))
+            print("----> [!] Exiting...")
+            exit()
 
-    subprocess.run(["systemctl", "restart", "knockd"])
+    print("[+] All systems go!")
 
 
 def populate_config(config, knockd_conf):
@@ -63,24 +129,48 @@ def populate_config(config, knockd_conf):
     return knockd_conf
 
 
+def write_to_knockd_conf(first_knock, second_knock, port, config):
+    global conf_iptables
+    conf_iptables_formatted = conf_iptables.format(
+        str(first_knock),
+        str(second_knock),
+        str(port))
+    with open("/etc/knockd.conf", "w+") as cnf:
+        cnf.write(conf_iptables_formatted)
+
+    subprocess.run(["systemctl", "restart", "knockd"])
+
+
 if __name__ == "__main__":
     """
-        Three arguments: ./knocker.py [OAUTH_SECRET] [PORT] [MODE]
+        Three arguments: ./knocker.py [OAUTH_SECRET] [PORT]
     """
-    if len(sys.argv) < 4:
-        print("Oh, I am going to need a little more than that.")
-        print("USAGE: ./knocker.py [OAUTH_SECRET] [PORT] [MODE]")
-        exit()
-    AUTH = sys.argv[1]
-    CODE = pyotp.TOTP(AUTH)
-    port = sys.argv[2]
-    mode = sys.argv[3]
+    PARSER = ArgumentParser(
+        description="Port knocking utility that leverages knockd and iptables")
+    PARSER.add_argument(
+        "-c",
+        "--config",
+        type=str,
+        required=True,
+        help="Path to Knocker config file.")
+    PARSER.add_argument(
+        "-f",
+        "--force",
+        action="store_true",
+        help="Skip checking of permissions and binaries")
 
+    ARGS = PARSER.parse_args()
+    if not ARGS.force:
+        check_sanity(ARGS.config)
+    else:
+        print("[!] Skipping sanity check. NOT SAFE.")
+
+    exit()
+    CODE = pyotp.TOTP(AUTH)
     # Initial Run.
     OTP = CODE.now()
     knock1, knock2 = get_knocks(int(OTP))
-    print("Knocks: \t%d\t%d" % (knock1, knock2))
-    write_to_knockd_conf(knock1, knock2, port, mode)
+    write_to_knockd_conf(knock1, knock2, port)
     last_otp = OTP
 
     while True:
@@ -92,7 +182,7 @@ if __name__ == "__main__":
                     continue
                 knock1, knock2 = get_knocks(int(OTP))
                 print("Knocks:\t%d\t%d" % (knock1, knock2))
-                write_to_knockd_conf(knock1, knock2, port, mode)
+                write_to_knockd_conf(knock1, knock2, port, config)
                 last_otp = OTP
         except KeyboardInterrupt:
             print("Exiting...")
